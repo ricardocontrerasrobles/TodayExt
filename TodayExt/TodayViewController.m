c9c4798172b7fd0246535b56f77f9f6a5230374c
Ricardo Contreras <ricardocontrerasrobles@gmail.com>
2015-03-09 18:40:12 -0600

    Cambio 1

--- TodayExt/TodayViewController.m
+++ TodayExt/TodayViewController.m
@@ -76,11 +76,6 @@
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
-    
-    // If an error is encountered, use NCUpdateResultFailed
-    // If there's no update required, use NCUpdateResultNoData
-    // If there's an update, use NCUpdateResultNewData
 
     completionHandler(NCUpdateResultNewData);
 }
